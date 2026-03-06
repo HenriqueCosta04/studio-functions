dce775abf964153bafb007ad70290761a3dbf2b4
HenriqueCosta04 <henrique.costa@olos.com.br>
2026-03-06 16:27:19 -0300

    ...

--- advanced_text_normalization_3.js
+++ advanced_text_normalization_3.js
@@ -1,3 +1,3 @@
-function normalizeText(str) {
-  return pre_defined_variable.replace(/\s+/g," ").replace(/[^\w\s]/g,"").replace(/\b\w/g,c=>c.toUpperCase()).trim().concat(str);
+function normalizeText(fnParameter) {
+  return pre_defined_variable.replace(/\s+/g," ").replace(/[^\w\s]/g,"").replace(/\b\w/g,c=>c.toUpperCase()).trim().concat(fnParameter);
 }
