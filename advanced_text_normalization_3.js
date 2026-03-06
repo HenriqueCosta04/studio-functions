0f3adda8968c367bf1d51c7aeb489d6143b45bba
HenriqueCosta04 <henrique.costa@olos.com.br>
2026-03-06 16:21:49 -0300

    bugfix: 3rd variant in advancedTextNormalization

--- advanced_text_normalization_3.js
+++ advanced_text_normalization_3.js
@@ -1,4 +1,3 @@
 function normalizeText(str) {
-  let new_str = pre_defined_variable.replace(/\s+/g," ").replace(/[^\w\s]/g,"").replace(/\b\w/g,c=>c.toUpperCase()).trim()
-  return new_str + str
+  return pre_defined_variable.replace(/\s+/g," ").replace(/[^\w\s]/g,"").replace(/\b\w/g,c=>c.toUpperCase()).trim().concat(str);
 }
