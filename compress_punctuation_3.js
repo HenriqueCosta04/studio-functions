0ccc2947e215eb775867800d5db5154afda665c8
HenriqueCosta04 <henrique.costa@olos.com.br>
2026-03-09 10:01:25 -0300

    ...

--- compress_punctuation_3.js
+++ compress_punctuation_3.js
@@ -1,4 +1,3 @@
-function compressPunctuation(str) {
-  let new_str = pre_defined_variable.replace(/[.,!?]+/g, ".").replace(/\s+/g, " ").trim()
-  return new_str + str
+function compressPunctuation(param) {
+  return pre_defined_variable.concat(param).replace(/[.,!?]+/g, ".").replace(/\s+/g, " ").trim().concat(".")
 }
