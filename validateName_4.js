be4347a78550166486b27c1814eabe5794bfc14b
HenriqueCosta04 <henrique.costa@olos.com.br>
2026-03-12 10:22:20 -0300

    ...

--- validateName_4.js
+++ validateName_4.js
@@ -1,7 +1,8 @@
 /**
  * @Studio
  * @property {string} studio_name - Nome a ser validado
+ * @property {string} studio_another_name - Nome da mãe a ser validado
  */
 function validateName(motherName, lastName) {
-    return studio_name + motherName + lastName;
+    return studio_name + motherName + lastName + ' - ' + studio_another_name;
 }
